c08206d0e463962c67e7005791d969d71a3051b6
Nicolás Jiménez <njimenezr24@fpcoruna.afundacion.org>
2024-10-11 17:41:51 +0200

    Hechos comentarios en la clase treasure

--- sprint0python/Game/treasure.py
+++ sprint0python/Game/treasure.py
@@ -1,21 +1,23 @@
-import Hero
+from django.template.defaultfilters import random
 import random
 
 class Treasure:
-    benefits = ["+Atk", "+Def","+HP"]
+    def __init__(self):
+        # Inicializa la lista de beneficios que puede encontrar el héroe
+        self.benefits = ["Potion of Strength", "Potion of Toughness", "Healing Potion"]
 
-    def find_treasure(self,Hero):
-        random_benefit = random.choice(Treasure.benefits)
-        print("Héroe ha encontrado un tesoro" + Treasure.benefits)
+    def find_treasure(self, hero):
+        # Selecciona un beneficio aleatorio de la lista de tesoros
+        random_benefit = random.choice(self.benefits)
+        print("The hero has found a treasure: " + random_benefit)
 
-        if random_benefit == "+Atk":
-            Hero.attack_dmg + 10
-            print("El ataque de" + Hero.name + "aumenta a"+Hero.attack_dmg)
-        elif random_benefit == "+Def":
-            Hero.defense_value +10
-            print("La defensa de" + Hero.name + "aumenta a"+Hero.defense_value)
-        elif random_benefit == "+HP":
-            Hero.health = Hero.max_health
-            print("La salud de" + Hero.name + "ha sido restaurada a "+Hero.max_health)
-
-
+        # Aplica el beneficio encontrado al héroe
+        if random_benefit == "Potion of Strength":
+            hero.hero_attack_dmg += 10  # Aumenta el daño de ataque del héroe
+            print(hero.hero_name + "'s attack increases to " + str(hero.hero_attack_dmg))
+        elif random_benefit == "Potion of Toughness":
+            hero.hero_defense_value += 10  # Aumenta la defensa del héroe
+            print(hero.hero_name + "'s defense increases to " + str(hero.hero_defense_value))
+        elif random_benefit == "Healing Potion":
+            hero.hero_health = hero.hero_max_health  # Restaura la salud del héroe
+            print(hero.hero_name + "'s health has been restored to " + str(hero.hero_max_health))
