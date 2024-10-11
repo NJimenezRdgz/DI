625fdba6ce7bf8716c1711daf13dcea064669a5b
Nicolás Jiménez <njimenezr24@fpcoruna.afundacion.org>
2024-10-11 17:41:30 +0200

    Hechos commentarios en la clase monster

--- sprint0python/Game/monster.py
+++ sprint0python/Game/monster.py
@@ -1,24 +1,31 @@
-import Hero
-
+from django.template.defaultfilters import random
+import random
 
 class Monster:
-    name = "Pepe"
-    attack_dmg = 10
-    defense_value = 10
-    health = 20
+    def __init__(self, monster_name):
+        # Inicializa las propiedades del monstruo
+        self.monster_name = monster_name
+        self.monster_attack_dmg = random.randint(5, 20)  # Daño de ataque aleatorio entre 5 y 20
+        self.monster_defense_value = random.randint(3, 10)  # Valor de defensa aleatorio entre 3 y 10
+        self.monster_health = random.randint(20, 40)  # Salud aleatoria entre 20 y 40
 
-    def attack(self,Hero):
-        damage = Monster.attack_dmg-Hero.defense_value
-        print("El monstruo ataca a " + Hero.name)
+    def attack(self, hero):
+        # Calcula el daño al héroe basándose en el ataque del monstruo y la defensa del héroe
+        damage = self.monster_attack_dmg - hero.hero_defense_value
+        print(self.monster_name + " attacks " + hero.hero_name)
+
         if damage > 0:
-
-            print("El héroe" + Hero.name + "ha recibido" + damage + "puntos de daño")
-
+            # Si el daño es positivo, se aplica al héroe
+            print(hero.hero_name + " has received " + str(damage) + " points of damage")
+            hero.hero_health -= damage
+            if hero.hero_health <= 0:
+                print("You've died :(")  # Mensaje si el héroe muere
+            else:
+                print("Current hero's health: " + str(hero.hero_health))
         else:
-            print("El héroe ha bloqueado el ataque")
+            # Si el daño es cero o negativo, el ataque ha sido bloqueado
+            print("The hero has blocked the attack")
 
     def is_alive(self):
-        if Monster.health>0:
-            return True
-        else:
-            return False
+        # Devuelve True si el monstruo está vivo, False si ha muerto
+        return self.monster_health > 0
