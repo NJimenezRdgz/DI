effe1eff582091c842081f02c416d3cb33afa9f0
Nicolás Jiménez <njimenezr24@fpcoruna.afundacion.org>
2024-10-11 17:40:21 +0200

    Hechas operaciones para ser realizadas con la calculadora, con commentarios

--- sprint0python/operaciones.py
+++ sprint0python/operaciones.py
@@ -1,14 +1,20 @@
-def suma(a,b):
+def suma(a, b):
+    # Devuelve la suma de a y b
     return a + b
-def resta(a,b):
-    return a-b
-def multiplicacion(a,b):
-   return a * b
 
+def resta(a, b):
+    # Devuelve la resta de a menos b
+    return a - b
 
-def division(a,b):
+def multiplicacion(a, b):
+    # Devuelve el producto de a y b
+    return a * b
+
+def division(a, b):
+    # Comprueba si b es cero antes de realizar la división
     if b == 0:
+        # Muestra un mensaje de error si b es cero
         return print("No se puede dividir entre 0")
-
     else:
+        # Devuelve el resultado de la división de a entre b
         return a / b
