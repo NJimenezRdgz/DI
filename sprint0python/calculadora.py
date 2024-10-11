a02026d4d44039cfaff2363019fac937c4305100
Nicolás Jiménez <njimenezr24@fpcoruna.afundacion.org>
2024-10-11 17:39:45 +0200

    Hecha calculadora con comentarios

--- sprint0python/calculadora.py
+++ sprint0python/calculadora.py
@@ -1,20 +1,31 @@
-from operaciones import suma, resta, multiplicacion,division
+from operaciones import suma, resta, multiplicacion, division
+
+
 respuesta = 's'
-while(respuesta == 's'):
+
+
+while respuesta == 's':
     print("Dame 2 valores")
+
+    # Solicita al usuario que ingrese dos valores
     a = int(input("Valor a: "))
     b = int(input("Valor b: "))
-    operacion = input("Selecciona una accion a realizar(suma, resta, multiplicacion, division): ")
 
-    
+    # Solicita al usuario que seleccione una operación
+    operacion = input("Selecciona una acción a realizar (suma, resta, multiplicacion, division): ")
+
+    # Ejecuta la operación seleccionada y muestra el resultado
     if operacion == 'suma':
-        print(suma(a,b))
+        print(suma(a, b))
     elif operacion == 'resta':
-        print(resta(a,b))
+        print(resta(a, b))
     elif operacion == 'multiplicacion':
-        print(multiplicacion(a,b))
+        print(multiplicacion(a, b))
     elif operacion == 'division':
-        print(division(a,b))
+        print(division(a, b))
     else:
-        print("No has seleccionado una accion valida")
-    respuesta = input("Quieres hacer mas operaciones?(s=si//n=no): ")
+        # Mensaje de error si la operación no es válida
+        print("No has seleccionado una acción válida")
+
+    # Pregunta al usuario si desea realizar más operaciones
+    respuesta = input("¿Quieres hacer más operaciones? (s=si//n=no): ")
