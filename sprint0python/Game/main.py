d9c5e658bf3871381f0ddbbfcc0a5f80c0f09f4f
Nicolás Jiménez <njimenezr24@fpcoruna.afundacion.org>
2024-10-11 17:42:21 +0200

    Hechos comentarios en main

--- sprint0python/Game/main.py
+++ sprint0python/Game/main.py
@@ -1,4 +1,13 @@
-import Hero
-import Monster
-import Treasure
-import Dungeon
+from Game.hero import Hero
+from Game.dungeon import Dungeon
+
+def main():
+    # solicita el nombre del héroe al usuario
+    hero_name = input("Name?: ")
+    hero = Hero(hero_name)  # Crea una instancia del héroe
+
+    dungeon = Dungeon(hero)  # Crea una instancia de la clase Dungeon
+    dungeon.play()  # Inicia el juego
+
+if __name__ == "__main__":
+    main()  # Llama a la función main
