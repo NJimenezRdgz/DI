e60bf7bb56e926037794f8aecabfcb7305548ee6
Nicolás Jiménez <njimenezr24@fpcoruna.afundacion.org>
2024-10-14 17:00:56 +0200

    Ejercicio 2ç

---
+++ sprint1tkinter/ejercicio2.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+
+root = tk.Tk()
+root.title("Mi ventana")
+root.geometry("300x200")
+def mostrar_mensaje():
+    etiqueta1 = tk.Label(root, text="Bienvenid@!")
+    etiqueta1.pack()
+def close():
+    root.destroy()
+
+
+boton1 = tk.Button(root, text= "Haz click aquí", command=mostrar_mensaje, bg="green", fg="white")
+boton1.pack(pady=50)
+
+boton2 = tk.Button(root, text= "Haz click para cerrar", command=close, bg="green", fg="white")
+boton2.pack(pady=10)
+
+
+root.mainloop()
