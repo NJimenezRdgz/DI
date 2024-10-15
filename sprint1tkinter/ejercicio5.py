1491ee8ea26d2b846851caa7a7ab65d3a2bc7bde
Nicolás Jiménez <njimenezr24@fpcoruna.afundacion.org>
2024-10-15 14:31:12 +0200

    Ejercicio 5

---
+++ sprint1tkinter/ejercicio5.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+
+
+
+root = tk.Tk()
+root.title("")
+root.geometry("600x400")
+
+def mostrar_seleccion():
+    seleccion = var_check.get()
+    root.config(background=seleccion)
+
+
+
+var_check= tk.StringVar()
+var_check.set("red")
+
+
+check1 = tk.Radiobutton(root, text="Rojo",variable = var_check,value ="red",command=mostrar_seleccion)
+check1.pack(pady=20)
+
+check2 = tk.Radiobutton(root, text="Verde",variable=var_check,value ="green",command=mostrar_seleccion)
+check2.pack(pady=21)
+
+check3 = tk.Radiobutton(root, text="Azul",variable=var_check,value ="blue",command=mostrar_seleccion)
+check3.pack(pady=23)
+
+
+
+
+
+root.mainloop()
