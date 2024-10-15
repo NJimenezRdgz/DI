a820633ccb8bb34f1f9933800ebd62be3809c2cc
Nicolás Jiménez <njimenezr24@fpcoruna.afundacion.org>
2024-10-15 14:44:07 +0200

    Ejercicio 6

---
+++ sprint1tkinter/ejercicio6.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from bisect import insort
+from pydoc import text
+
+
+def mostrar_selecciones():
+    seleccion = listbox.curselection()
+    elementos = [listbox.get(i) for i in seleccion]
+
+    etiqueta.config(text = f"Seleccionaste: {', '.join(elementos)}")
+
+root = tk.Tk()
+root.title("Ejemplo Listbox")
+root.geometry("300x250")
+
+opciones = ["Manzana", "Banana","Naranja"]
+
+listbox = tk.Listbox(root, selectmode= tk.SINGLE)
+for opcion in opciones:
+    listbox.insert(tk.END, opcion)
+listbox.pack(pady=10)
+
+boton = tk.Button(root, text="Mostrar Selección", command=mostrar_selecciones)
+boton.pack(pady=5)
+
+etiqueta = tk.Label(root, text="Seleccionaste: Ninguno")
+etiqueta.pack(pady=10)
+
+root.mainloop()
