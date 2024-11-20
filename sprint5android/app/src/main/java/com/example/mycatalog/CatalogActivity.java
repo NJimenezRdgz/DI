1d3a6044d134bd1213ca266ef84d239dcbe4e6fd
Nicolás Jiménez <njimenezr24@fpcoruna.afundacion.org>
2024-11-20 10:01:27 +0100

    Agregado botón y creado funcionalidad para conectarlo con detailactivity

--- sprint5android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint5android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,24 +1,31 @@
-package com.example.mycatalog;
+    package com.example.mycatalog;
 
-import android.os.Bundle;
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+    import androidx.activity.EdgeToEdge;
+    import androidx.appcompat.app.AppCompatActivity;
+    import androidx.core.graphics.Insets;
+    import androidx.core.view.ViewCompat;
+    import androidx.core.view.WindowInsetsCompat;
 
-public class CatalogActivity extends AppCompatActivity {
+    public class CatalogActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            EdgeToEdge.enable(this);
+            setContentView(R.layout.activity_catalog);
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+            Button botonNavegar = findViewById(R.id.boton_navegar);
+            botonNavegar.setOnClickListener(v -> {
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+                startActivity(intent);
+            });
+        }
     }
-}
