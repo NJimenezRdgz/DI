d6a8194233e7e9dada984a4fd157873fb2533d99
Nicolás Jiménez <njimenezr24@fpcoruna.afundacion.org>
2024-11-22 09:57:50 +0100

    Ejercicio 4

--- sprint5android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint5android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -1,12 +1,17 @@
 package com.example.mycatalog;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
-
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.time.Instant;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -20,5 +25,20 @@ public class DetailActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        ImageView imageView = findViewById(R.id.detail_imagen_circular);
+        int orientation = getResources().getConfiguration().orientation;
+
+// si es horizontal cambiar el layout a "detail_activity.xml".
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE)
+            setContentView(R.layout.detail_activity);
+        else
+            // si es vertical usar "activity_detail.xml".
+            setContentView(R.layout.activity_detail);
+
+        Glide.with(this)
+                .load("https://example.com/image.jpg")
+                .apply(RequestOptions.circleCropTransform()) // Recorta en círculo
+                .placeholder(R.drawable.placeholder)
+                .into(imageView);
     }
 }
